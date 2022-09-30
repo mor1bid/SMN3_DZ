8bdebd3260f5785abf5e92a570b9823c2eb94bac
De <spark49942@gmail.com>
2022-09-30 16:55:53 +0300

    master

--- smn_dz.py
+++ smn_dz.py
@@ -1,13 +1,15 @@
-# a - открытие для добавления данных; r - открытие для чтения; w - открытие для записи данных
+# a - открытие для добавления данных (из файла); r - открытие для чтения; w - открытие для записи данных
 # w+, r+
 colors = ['red', 'blue', 'green']
 data = open('txt.txt', 'a')
-data.writelines(colors, ',')
+data.writelines(colors)
+data.write('\nКошмары\n')
+data.write('\nИнтернета\n')
 data.close()
 
 
 path = 'txt.txt'
 data = open(path, 'r')
 for line in data:
-    print(line)
+    print(line, '\n')
 data.close()
