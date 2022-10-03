84fa0326c0434b09eada824ba724d061a9862bdc
De <spark49942@gmail.com>
2022-10-03 03:28:19 +0300

    DZ312f

--- smn_dz.py
+++ smn_dz.py
@@ -1,4 +1,4 @@
-si = int(input('1. Задайте длинну списка: '))
+si = int(input('1. Задайте длину списка: '))
 ray = []
 res = 0
 print('Введите', si, 'элемента(-ов) списка:')
@@ -9,17 +9,25 @@ print(ray)
 for i in range(si):
     if i%2!=0:
         res += ray[i]
-print('Сумма элементов данного массива =', res)
+print('Сумма элементов данного списка =', res)
 
-# ze = int(si/2)
 i2 = 0
 ar = []
-print('2. Произведение пар элементов данного массива =')
+print('2. Произведение пар элементов на нечётных позициях в данном списке =', end=' ')
 for i in range(si):
     if i<si/2:
         i2-=1
         res= ray[i]*ray[i2]
         ar.append(res)
 print(ar)
-    
 
+strip = [1.1, 1.2, 3.1, 5, 10.01]
+big = 0
+tin = 0
+print('3. Разность между максимальным и минимальным значениями данного списка =', end='')
+for i in strip:
+    if i%1>big:
+        big=i
+    elif i%1<tin:
+        tin=i
+print(strip, '-', big, '-', tin, '=', big-tin)
