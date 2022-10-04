92da389e885682e8763176b7413e97aed3f40f66
De <spark49942@gmail.com>
2022-10-04 04:08:08 +0300

    DZ-a

--- smn_dz.py
+++ smn_dz.py
@@ -1,40 +1,40 @@
-# si = int(input('1. Задайте длину списка: '))
-# ray = []
-# res = 0
-# print('Введите', si, 'элемента(-ов) списка:')
-# for i in range(si):
-#     num = int(input())
-#     ray.append(num)
-# print(ray)
-# for i in range(si):
-#     if i%2!=0:
-#         res += ray[i]
-# print('Сумма элементов на нечётных позициях в данном списке =', res)
+si = int(input('1. Задайте длину списка: '))
+ray = []
+res = 0
+print('Введите', si, 'элемента(-ов) списка:')
+for i in range(si):
+    num = int(input())
+    ray.append(num)
+print(ray)
+for i in range(si):
+    if i%2!=0:
+        res += ray[i]
+print('Сумма элементов на нечётных позициях в данном списке =', res)
 
-# i2 = 0
-# ar = []
-# print('2. Произведение пар элементов в данном списке =', end=' ')
-# for i in range(si):
-#     if i<si/2:
-#         i2-=1
-#         res= ray[i]*ray[i2]
-#         ar.append(res)
-# print(ar)
+i2 = 0
+ar = []
+print('2. Произведение пар элементов в данном списке =', end=' ')
+for i in range(si):
+    if i<si/2:
+        i2-=1
+        res= ray[i]*ray[i2]
+        ar.append(res)
+print(ar)
 
-# import random
-# strip = []
-# for i in range(si):
-#     num = round(random.uniform(-10.1, 10.1), 2)
-#     strip.append(num)
-# big = strip[0]
-# tin = strip[0]
-# print('3.', strip, '\n' 'Разность между максимальным и минимальным значениями данного списка =', end=' ')
-# for i in strip:
-#     if i%1>=big%1:
-#         big=i
-#     elif i%1<=tin%1 and i%1!=0:
-#         tin=i
-# print(big, '-', tin, '=', round((big%1)-(tin%1), 2))
+import random
+strip = []
+for i in range(si):
+    num = round(random.uniform(-10.1, 10.1), 2)
+    strip.append(num)
+big = strip[0]
+tin = strip[0]
+print('3.', strip, '\n' 'Разность между максимальным и минимальным значениями данного списка =', end=' ')
+for i in strip:
+    if i%1>=big%1:
+        big=i
+    elif i%1<=tin%1 and i%1!=0:
+        tin=i
+print(big, '-', tin, '=', round((big%1)-(tin%1), 2))
 
 num = input('4. Введите число, которое желаете перевести в двоичное: ')
 bin = []
@@ -52,20 +52,19 @@ for i in num:
 for i in reversed(bin):
     print(i, end='')
 
+n = int(input('\n' '5. Введите число: '))
+def fib(n):
+    if n in [1, 2]:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
 
-# n = int(input('5. Введите число: '))
-# def fib(n):
-#     if n in [1, 2]:
-#         return 1
-#     else:
-#         return fib(n-1)+fib(n-2)
-
-# plusl = []
-# minusl = []
-# for i in range(1, n):
-#     plusl.append(fib(i))
-#     minusl.append(fib(i)*-1)
-# minusl.sort(reverse=False)
-# for i in plusl:
-#     minusl.append(i)
-# print(minusl)
+plusl = []
+minusl = []
+for i in range(1, n):
+    plusl.append(fib(i))
+    minusl.append(fib(i)*-1)
+minusl.sort(reverse=False)
+for i in plusl:
+    minusl.append(i)
+print(minusl)
